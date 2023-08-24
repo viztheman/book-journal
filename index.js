69300dc8100ad4d142b5380d6684a615ce82993b
Daniel Hammond <viztheman@protonmail.com>
2023-08-23 21:10:54 -0400

    Added Mongoose models and connections.

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 require('dotenv').config();
-const {PORT} = process.env;
+const {PORT, MONGODB} = process.env;
 
 const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
+const mongoose = require('mongoose');
 
-const app = express();
-app.set('view engine', 'pug');
-app.use(helmet());
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/@popperjs/core/dist/umd')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+main().catch(e => console.error(e));
 
-app.use('/', require('./lib/routers/home'));
-app.use('/add', require('./lib/routers/add'));
-app.use('/edit', require('./lib/routers/edit'));
-app.use('/details', require('./lib/routers/details'));
+async function main() {
+	await mongoose.connect(MONGODB);
 
-app.listen(PORT, () => console.log(`Book Journal listening on port ${PORT}`));
+	const app = express();
+	app.set('view engine', 'pug');
+	app.use(helmet());
+	app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+	app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+	app.use('/js', express.static(path.join(__dirname, 'node_modules/@popperjs/core/dist/umd')));
+	app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+
+	app.use('/', require('./lib/routers/home'));
+	app.use('/add', require('./lib/routers/add'));
+	app.use('/edit', require('./lib/routers/edit'));
+	app.use('/details', require('./lib/routers/details'));
+
+	app.listen(PORT, () => console.log(`Book Journal listening on port ${PORT}`));
+}
